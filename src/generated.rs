92f8ab5a9f87cfa4e922a9384f7b348d206cc877
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2023-02-17 18:00:03 +0100

    chore: modify build process to use precompiled flatbuffers schema
    This commit removes schema compilation, but instead uses a schema from
    the releases section over at `rman-schema`. Submodule `rman-schema` is
    no longer being used, however it will stay linked and updated as a way
    to track which version of the schema is being used.

---
+++ src/generated.rs
@@ -0,0 +1,6 @@
+#[allow(warnings)]
+#[allow(missing_debug_implementations, missing_copy_implementations)]
+#[allow(clippy::all, clippy::pedantic, clippy::nursery, clippy::unwrap_used)]
+mod schema_generated;
+
+pub use schema_generated::rman;
